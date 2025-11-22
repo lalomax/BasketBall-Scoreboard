bf645d4e1641c1d3c748c441dd5a8714fa50dc40
Lalomax <lalomax365@gmail.com>
2025-11-21 20:55:58 -0400

    perf: adding timer and improve style

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
-const markerHome = document.getElementById("home-result")
-const markerGuest = document.getElementById("guest-result")
+const markerHome = document.getElementById("home-result");
+const markerGuest = document.getElementById("guest-result");
+const timerElement = document.getElementById("game-timer");
+let time = 0;
+let timerInterval;
 
-put = (e) => { //e represents the current object, on wich user interacts
-  if (e.name === "home") { 
-    markerHome.innerHTML = parseInt(markerHome.innerHTML) + parseInt(e.value)
+put = (e) => {
+  //e represents the current object, on wich user interacts
+  if (e.name === "home") {
+    markerHome.innerHTML = parseInt(markerHome.innerHTML) + parseInt(e.value);
   } else {
-    markerGuest.innerHTML = parseInt(markerGuest.innerHTML) + parseInt(e.value)
+    markerGuest.innerHTML = parseInt(markerGuest.innerHTML) + parseInt(e.value);
   }
+};
+
+
+
+make = (e) => {
+  if (e.name === "home") {
+    markerHome.innerHTML = "0";
+  } else {
+    markerGuest.innerHTML = "0";
+  }
+};
+
+function startTimer() {
+  clearInterval(timerInterval);
+  timerInterval = setInterval(() => {
+    time++;
+    updateTimer();
+  }, 1000);
 }
 
-make = (e) => { 
-    if (e.name === "home") {
-    markerHome.innerHTML = "0"
-  } else {
-    markerGuest.innerHTML = "0"
-  }
+function stopTimer() {
+  clearInterval(timerInterval);
 }
+
+function resetTimer() {
+  clearInterval(timerInterval);
+  time = 0;
+  updateTimer();
+}
+
+function updateTimer() {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  timerElement.textContent = `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
+}
+updateTimer();
